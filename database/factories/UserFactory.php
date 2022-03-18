93c301ef279d7a0eb8614b5119b960861f53cbf0
tolos123 <alemchu123@yahoo.com>
2022-03-18 01:11:21 +0100

    add global email

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,7 +17,8 @@ class UserFactory extends Factory
         return [
             'first_name' => $this->faker->name(),
             'last_name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            // 'email' => $this->faker->unique()->safeEmail(),
+            'email' => 'test@test.com',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'pwd_changed' => true
 
