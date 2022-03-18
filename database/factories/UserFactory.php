f7f8202b391b955390ac233e3d83293277976636
tolos123 <alemchu123@yahoo.com>
2022-03-18 01:10:12 +0100

    update user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,6 +19,8 @@ class UserFactory extends Factory
             'last_name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'pwd_changed' => true
+
         ];
     }
 
